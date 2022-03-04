a1cb0ba874377e1b39268a6f8b180c094dac377b
bozgen <bilalozgen00@gmail.com>
2022-03-04 15:51:47 +0300

    initial commit

---
+++ HelloWorld/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hello-world',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ HelloWorld/src/components/ExploreContainer.tsx
@@ -0,0 +1,13 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Hello World!</strong>
+    </div>
+  );
+};
+
+export default ExploreContainer;
